2b43bf6e1227f244d1257fbdad6d828721f2cc63
0xAckerMan <joelkores1@gmail.com>
2023-05-12 11:31:47 +0300

    Initial commit

---
+++ wp-content/plugins/myassessment/inc/init.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Myassessment
+ */
+
+namespace Inc;
+
+final class init{
+    /**
+     * store all the content inside an array
+     *
+     * @return array full of classes
+     */
+    public static function get_services(){
+        return [
+            Pages\Admin::class,
+            Base\Enqueue::class,
+            Pages\RegisterMember::class
+        ];
+    }
+    public static function register_services(){
+        foreach (self::get_services() as $class){
+            $service = self::instantiate($class);
+            if(method_exists($service, 'register')){
+                $service -> register();
+            }
+        }
+    }
+
+    private static function instantiate($class){
+        $service = new $class();
+
+        return $service;
+    }
+}
